84d51d11f9ed5e9dd6c11066d899c3b542485bb5
Jerry Needell <jerryneedell@gmail.com>
2020-04-12 10:39:46 -0400

    create isrtest - simple directory structure

---
+++ isrtest/Debug/CMSIS/startup_gcc/subdir.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../CMSIS/startup_gcc/newlib_stubs.c 
+
+S_UPPER_SRCS += \
+../CMSIS/startup_gcc/startup_a2fxxxm3.S 
+
+OBJS += \
+./CMSIS/startup_gcc/newlib_stubs.o \
+./CMSIS/startup_gcc/startup_a2fxxxm3.o 
+
+S_UPPER_DEPS += \
+./CMSIS/startup_gcc/startup_a2fxxxm3.d 
+
+C_DEPS += \
+./CMSIS/startup_gcc/newlib_stubs.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+CMSIS/startup_gcc/%.o: ../CMSIS/startup_gcc/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU ARM Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g3 -DACTEL_STDIO_THRU_UART -I../ -I../CMSIS -I../CMSIS/startup_gcc  -I../drivers/mss_watchdog -I../drivers/mss_uart -std=gnu11 --specs=nano.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+CMSIS/startup_gcc/%.o: ../CMSIS/startup_gcc/%.S
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU ARM Cross Assembler'
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g3 -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ isrtest/Debug/drivers/mss_uart/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../drivers/mss_uart/mss_uart.c 
+
+OBJS += \
+./drivers/mss_uart/mss_uart.o 
+
+C_DEPS += \
+./drivers/mss_uart/mss_uart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+drivers/mss_uart/%.o: ../drivers/mss_uart/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU ARM Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g3 -DACTEL_STDIO_THRU_UART -I../ -I../CMSIS -I../CMSIS/startup_gcc -I../drivers/mss_watchdog -I../drivers/mss_uart -std=gnu11 --specs=nano.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
