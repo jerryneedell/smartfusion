12a6ab7e930f7da72c97d356fe23fd40ec9133b8
jerryneedell <jerryneedell@gmail.com>
2021-03-31 12:20:15 -0400

    file cleanup

--- smartfusion2/SWFO_SCSim/SCSim/Debug/drivers/mss_gpio/mss_gpio.d
+++
@@ -1,7 +0,0 @@
-drivers/mss_gpio/mss_gpio.o: ../drivers/mss_gpio/mss_gpio.c \
- ../drivers/mss_gpio/mss_gpio.h ../drivers/mss_gpio/../../CMSIS/m2sxxx.h \
- ../CMSIS/core_cm3.h ../drivers/mss_gpio/../../CMSIS/mss_assert.h
-../drivers/mss_gpio/mss_gpio.h:
-../drivers/mss_gpio/../../CMSIS/m2sxxx.h:
-../CMSIS/core_cm3.h:
-../drivers/mss_gpio/../../CMSIS/mss_assert.h:
