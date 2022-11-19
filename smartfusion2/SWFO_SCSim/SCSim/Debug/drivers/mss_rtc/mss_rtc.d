f65ff815385ce90581d82177b3d3b2b56f46e502
jerryneedell <jerryneedell@gmail.com>
2022-11-19 11:07:39 -0500

    cleanup unwanted files

--- smartfusion2/SWFO_SCSim/SCSim/Debug/drivers/mss_rtc/mss_rtc.d
+++
@@ -1,7 +0,0 @@
-drivers/mss_rtc/mss_rtc.o: ../drivers/mss_rtc/mss_rtc.c \
- ../drivers/mss_rtc/mss_rtc.h ../drivers/mss_rtc/../../CMSIS/m2sxxx.h \
- ../CMSIS/core_cm3.h ../drivers/mss_rtc/../../CMSIS/mss_assert.h
-../drivers/mss_rtc/mss_rtc.h:
-../drivers/mss_rtc/../../CMSIS/m2sxxx.h:
-../CMSIS/core_cm3.h:
-../drivers/mss_rtc/../../CMSIS/mss_assert.h:
